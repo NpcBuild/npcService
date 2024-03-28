e9385c71a2a8fd1ec02fcec3900a7c16b96628e0
yangfei <zhazhafei@aliyun.com>
2024-03-28 22:09:58 +0800

    rsa加密工具整理代码

--- system/src/main/java/com/npc/core/encrypt/RSA/RsaCryptUtil.java
+++ system/src/main/java/com/npc/core/encrypt/RSA/RsaCryptUtil.java
@@ -21,6 +21,7 @@ public class RsaCryptUtil {
     public static final String PUB_KEY = "rsaKey/public.key" ;
     public static final String PRI_KEY = "rsaKey/private.key" ;
 
+    //    ------------------------------密钥字符串获取Start------------------------------
     /**
      * 密钥字符串获取
      * @return
@@ -43,6 +44,77 @@ public class RsaCryptUtil {
         return keyPairMap ;
     }
 
+    /**
+     * 文件加载
+     * @param keyPlace
+     * @return
+     * @throws Exception
+     */
+    public static String getKey (String keyPlace) throws Exception {
+        BufferedReader br= null;
+        try {
+            br= new BufferedReader(new InputStreamReader(RsaCryptUtil.class.getClassLoader().
+                    getResourceAsStream(keyPlace)));
+            String readLine= null;
+            StringBuilder keyValue = new StringBuilder();
+            while((readLine= br.readLine())!=null){
+                if(!(readLine.charAt(0)=='-')){
+                    keyValue.append(readLine);
+                }
+            }
+            return keyValue.toString();
+        } catch (Exception e) {
+            throw new Exception("RSA密钥读取错误",e) ;
+        } finally{
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    System.out.println("密钥读取流关闭异常");
+                }
+            }
+        }
+    }
+    //    ------------------------------密钥字符串获取End------------------------------
+
+    //    ------------------------------公钥和私钥Start------------------------------
+    /**
+     * 公钥字符串生成公钥
+     * @param publicKeyValue
+     * @return
+     * @throws Exception
+     */
+    public static RSAPublicKey createPublicKey(String publicKeyValue) throws Exception {
+        try {
+            byte[] buffer = DatatypeConverter.parseBase64Binary(publicKeyValue);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        } catch (Exception e) {
+            throw new Exception("公钥创建失败", e);
+        }
+    }
+
+
+    /**
+     * 私钥字符串生成私钥
+     * @param privateKeyValue
+     * @return
+     * @throws Exception
+     */
+    public static RSAPrivateKey createPrivateKey(String privateKeyValue) throws Exception {
+        try {
+            byte[] buffer = javax.xml.bind.DatatypeConverter.parseBase64Binary(privateKeyValue);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        } catch (Exception e) {
+            throw new Exception("私钥创建失败", e);
+        }
+    }
+    //    ------------------------------公钥和私钥End------------------------------
+
+    //    ------------------------------加密和解密Start------------------------------
     /**
      * 公钥加密
      * @param publicKey
@@ -84,7 +156,9 @@ public class RsaCryptUtil {
             throw new Exception("私钥解密失败",e);
         }
     }
+    //    ------------------------------加密和解密End------------------------------
 
+    //    ------------------------------签名和验签Start------------------------------
     /**
      * 私钥签名
      * @param signData
@@ -121,7 +195,9 @@ public class RsaCryptUtil {
         signature.update(srcData.getBytes());
         return signature.verify(parseBase64Binary(sign));
     }
+    //    ------------------------------签名和验签End------------------------------
 
+    //    ------------------------------编码和解码Start------------------------------
     /**
      * 字节数组转字符
      */
@@ -134,70 +210,70 @@ public class RsaCryptUtil {
     public static byte[] parseBase64Binary(String value) {
         return DatatypeConverter.parseBase64Binary(value);
     }
+    //    ------------------------------编码和解码End------------------------------
 
+    //    ------------------------------测试代码块Start------------------------------
     /**
-     * 文件加载
-     * @param keyPlace
-     * @return
+     * 密钥生成测试
      * @throws Exception
      */
-    public static String getKey (String keyPlace) throws Exception {
-        BufferedReader br= null;
-        try {
-            br= new BufferedReader(new InputStreamReader(RsaCryptUtil.class.getClassLoader().
-                    getResourceAsStream(keyPlace)));
-            String readLine= null;
-            StringBuilder keyValue = new StringBuilder();
-            while((readLine= br.readLine())!=null){
-                if(!(readLine.charAt(0)=='-')){
-                    keyValue.append(readLine);
-                }
-            }
-            return keyValue.toString();
-        } catch (Exception e) {
-            throw new Exception("RSA密钥读取错误",e) ;
-        } finally{
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (Exception e) {
-                    System.out.println("密钥读取流关闭异常");
-                }
-            }
-        }
+    public static void testCreateKey () throws Exception {
+        HashMap<String, String> map = RsaCryptUtil.getTheKeys();
+        String privateKeyStr=map.get("privateKey");
+        String publicKeyStr=map.get("publicKey");
+        System.out.println("私钥："+privateKeyStr);
+        System.out.println("公钥："+publicKeyStr);
+        //消息发送方
+        String originData="cicada-smile";
+        System.out.println("原文："+originData);
+        String encryptData = RsaCryptUtil.encrypt(RsaCryptUtil.createPublicKey(publicKeyStr),
+                originData.getBytes());
+        System.out.println("加密："+encryptData);
+        //消息接收方
+        String decryptData=RsaCryptUtil.decrypt(RsaCryptUtil.createPrivateKey(privateKeyStr),
+                RsaCryptUtil.parseBase64Binary(encryptData));
+        System.out.println("解密："+decryptData);
     }
 
     /**
-     * 公钥字符串生成公钥
-     * @param publicKeyValue
-     * @return
+     * 密钥读取测试
      * @throws Exception
      */
-    public static RSAPublicKey createPublicKey(String publicKeyValue) throws Exception {
-        try {
-            byte[] buffer = DatatypeConverter.parseBase64Binary(publicKeyValue);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
-            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
-        } catch (Exception e) {
-            throw new Exception("公钥创建失败", e);
-        }
+    public static void testReadKey () throws Exception {
+        String value = getKey("rsaKey/public.key");
+        System.out.println(value);
+        String privateKeyStr = getKey(RsaCryptUtil.PRI_KEY) ;
+        String publicKeyStr = getKey(RsaCryptUtil.PUB_KEY) ;
+        //消息发送方
+        String originData="cicada-smile";
+        System.out.println("原文："+originData);
+        String encryptData = RsaCryptUtil.encrypt(RsaCryptUtil.createPublicKey(publicKeyStr),
+                originData.getBytes());
+        System.out.println("加密："+encryptData);
+        //消息接收方
+        String decryptData=RsaCryptUtil.decrypt(RsaCryptUtil.createPrivateKey(privateKeyStr),
+                RsaCryptUtil.parseBase64Binary(encryptData));
+        System.out.println("解密："+decryptData);
     }
 
     /**
-     * 私钥字符串生成私钥
-     * @param privateKeyValue
-     * @return
+     * 签名验签测试
      * @throws Exception
      */
-    public static RSAPrivateKey createPrivateKey(String privateKeyValue) throws Exception {
-        try {
-            byte[] buffer = javax.xml.bind.DatatypeConverter.parseBase64Binary(privateKeyValue);
-            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
-        } catch (Exception e) {
-            throw new Exception("私钥创建失败", e);
-        }
+    public static void testSignVerify () throws Exception {
+        String signData = "cicada-smile" ;
+        String privateKeyStr = getKey(RsaCryptUtil.PRI_KEY) ;
+        String publicKeyStr = getKey(RsaCryptUtil.PUB_KEY) ;
+        String signValue = sign(signData,RsaCryptUtil.createPrivateKey(privateKeyStr)) ;
+        boolean flag = verify(signData,RsaCryptUtil.createPublicKey(publicKeyStr),signValue);
+        System.out.println("原文:"+signData);
+        System.out.println("签名:"+signValue);
+        System.out.println("验签:"+flag);
     }
+
+    public static void main(String[] args) throws Exception {
+        testCreateKey();
+    }
+    //    ------------------------------测试代码块End------------------------------
+
 }
